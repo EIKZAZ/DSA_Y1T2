038fb044c896b269a870a04eddf66c13a2742eb1
Pakin Junjumlong <124805125+EIKZAZ@users.noreply.github.com>
2023-02-17 15:28:51 +0700

    Add files via upload

--- LAB043.py
+++ LAB043.py
@@ -31,7 +31,8 @@ class ArrayStack:
     def printStack(self):
         print(self.data)
 
-def infixToPostfix(expression, postfix=""):
+def infixToPostfix(expression):
+    postfix = ""
     stack = ArrayStack()
     for i in expression:
         if i.isalpha():
@@ -44,33 +45,13 @@ def infixToPostfix(expression, postfix=""):
                     while not stack.is_empty():
                         postfix += stack.pop()
                 stack.push(i)
+                # if i in "*/":
+                #     if stack.stackTop() in "*/":
+                #       postfix += stack.pop()  
     while not stack.is_empty():
         postfix += stack.pop()
     return postfix
 
-#def infixtopostfox(exp):
-    stack = ArrayStack()
-    operator = ['+', '-', '*', '/']
-    priority = {'*':2, '/':2, '+':1, '-':1}
-    ans = ''
-    for i in exp:
-        if i in operator:
-            if stack.is_empty():
-                stack.push(i)
-            else:
-                if priority.get(stack.stackTop()) > priority.get(i):
-                    while not stack.is_empty():
-                        x = stack.pop()
-                        ans += str(x)
-                    stack.push(i)
-                else:
-                    stack.push(i)
-        else:
-            ans += i
-    while not stack.is_empty():
-        x = stack.pop()
-        ans += str(x)
-    return ans
-exp = "A+B*C-D/E"
-postfix = infixToPostfix(exp)
-print("Postfix of", exp, "is", postfix)
+exp = "A+B*C/D-E+F*G"
+postfix2 = infixToPostfix(exp)
+print("Postfix of", exp, "is", postfix2)
